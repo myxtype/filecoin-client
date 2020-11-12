6d500cf77463ca8b6fb18cf45a0e60e7a3c599f5
zhaobo <xtype@qq.com>
2020-11-12 14:04:13 +0800

    Update

--- local/wallet_test.go
+++ local/wallet_test.go
@@ -22,3 +22,16 @@ func TestLocalWalletNew(t *testing.T) {
 	// baf063287d69520fe17d6065446a7b8a200799d676ba6844c8bfdca5dd3deb5c
 	// f3qmpxmbmdjkqk73zacadt3dkt3j5ftyi4kim5bg3ubujes7gk3w6dnvrndos6wbec2rlpwimk76zu5lwl6sma
 }
+
+func TestLocalWalletNew2(t *testing.T) {
+	// 指定节点网络
+	address.CurrentNetwork = address.Mainnet
+	// 生成地址对
+	ki, addr, err := LocalWalletNew(types.KTSecp256k1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Log(hex.EncodeToString(ki.PrivateKey))
+	t.Log(addr.String())
+}
